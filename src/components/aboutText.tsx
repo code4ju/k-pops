448d4dd34ad280535844bdc6414fcda83b3122c9
Ju Chang <code4ju@gmail.com>
2025-01-12 15:37:12 +0100

    created aboutText

--- src/components/aboutText.tsx
+++ src/components/aboutText.tsx
@@ -4,6 +4,7 @@ export const AboutText = () => {
 			<div className="flex flex-col w-4/6 self-end">
 				<div className="">
 					<p className="text-5xl">
+						<span className="text-sm px-12 align-top ">ABOUT</span>
 						Ethan Suero is an independent designer focused on
 						crafting immersive digital experiences. He believes
 						every project is an opportunity to deliver a unique and
