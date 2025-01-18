30d9059f5082b487237e47a48f00e1c3d6db7910
Ju Chang <code4ju@gmail.com>
2025-01-18 19:19:52 +0100

    refactor Gallery.tsx for mobile

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,32 +1,32 @@
 export const Gallery = () => {
 	return (
 		<>
-			<div className="flex flex-col items-center gap-32 max-w-screen-xl">
-				<h1 className="text-9xl">SELECTED</h1>
-				<div className="flex flex-row justify-between w-full">
-					<div className="self-center px-20">
+			<div className="flex flex-col gap-12">
+				<h1 className="text-6xl self-center">SELECTED</h1>
+				<div className="flex flex-col-reverse justify-between w-full">
+					<div className="flex flex-col p-3">
 						<h4 className="text-3xl">HOUSTON EXPONENTIAL</h4>
-						<p className="text-lg">
+						<p className="text-xl">
 							A new digital HUB for houston’s biggest startup
 							ecosystem.
 						</p>
 					</div>
 					<img
-						className="w-1/2"
+						className="p-3"
 						src="./img/assets/GalleryHouston-Exponential1.png"
 						alt=""
 					/>
 				</div>
 				<div>
-					<div className="flex flex-row justify-between w-full">
+					<div className="flex flex-col justify-between w-full">
 						<img
-							className="w-1/2"
+							className="p-3"
 							src="./img/assets/GalleryNami-ML.png"
 							alt=""
 						/>
-						<div className="self-center px-20 w-1/2">
+						<div className="flex flex-col p-3">
 							<h4 className="text-3xl">NAMI ML</h4>
-							<p className="text-lg">
+							<p className="text-xl">
 								A brand new digital identity and website for the
 								subscription App service that focuses on happy
 								subscribers.
@@ -35,16 +35,16 @@ export const Gallery = () => {
 					</div>
 				</div>
 				<div>
-					<div className="flex flex-row justify-between w-full">
-						<div className="self-center px-20 w-1/2">
+					<div className="flex flex-col-reverse justify-between w-full">
+						<div className="flex flex-col p-3">
 							<h4 className="text-3xl">THIN REEL</h4>
-							<p className="text-lg">
+							<p className="text-xl">
 								How we turned a local studio into one of the
 								biggest video agencies in the south of the UK.
 							</p>
 						</div>
 						<img
-							className="w-1/2"
+							className="p-3"
 							src="./img/assets/GalleryThin-Reel.png"
 							alt=""
 						/>
