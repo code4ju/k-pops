c4dc418b24ff57719a8c9f3981a9257ab7bc54ca
Ju Chang <code4ju@gmail.com>
2025-01-18 19:04:25 +0100

    refactor AboutText.tsx for mobile

--- src/components/aboutText.tsx
+++ src/components/aboutText.tsx
@@ -1,17 +1,18 @@
 export const AboutText = () => {
 	return (
 		<>
-			<div>
-				<div>
-					<p>
-						<span>ABOUT</span>
-						Ethan Suero is an independent designer focused on
-						crafting immersive digital experiences. He believes
-						every project is an opportunity to deliver a unique and
-						memorable digital experience that delights users and
-						builds brand equity.
-					</p>
-				</div>
+			<div className="p-3">
+				<p className="text-3xl">
+					<div className="inline">
+						<span className="align-text-top text-sm px-8">
+							ABOUT
+						</span>
+					</div>
+					Ethan Suero is an independent designer focused on crafting
+					immersive digital experiences. He believes every project is
+					an opportunity to deliver a unique and memorable digital
+					experience that delights users and builds brand equity.
+				</p>
 			</div>
 		</>
 	);
